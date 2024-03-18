c17e0a809a90aca259af01290ce47ffa644e0777
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-18 13:24:09 -0400

    create userchoice function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
 // function to randomly return "rock", "paper", or "scissors".
 function getComputerChoice(){
-   let comInt = Math.floor(Math.random() * 3) + 1
-   if (comInt == 1) {
+   let rndInt = Math.floor(Math.random() * 3) + 1
+   if (rndInt == 1) {
     return "rock";
-   } else if (comInt == 2) {
+   } else if (rndInt == 2) {
     return "paper";
    } else {
     return "scissors";
    }
-   return getComputerChoice;
+}
+
+//function for users to select their choice
+function getUserChoice () {
+    let UserChoice = prompt ("Rock, paper, or scissors?");
+    return UserChoice;
 }
