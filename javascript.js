ff852f49abaab86e2778b87ff550b4e1f646d865
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-18 13:27:40 -0400

    optimize computerchoice function

--- javascript.js
+++ javascript.js
@@ -1,17 +1,18 @@
 // function to randomly return "rock", "paper", or "scissors".
 function getComputerChoice(){
-   let rndInt = Math.floor(Math.random() * 3) + 1
-   if (rndInt == 1) {
-    return "rock";
-   } else if (rndInt == 2) {
-    return "paper";
+   let computerChoice = Math.floor(Math.random() * 3) + 1
+   if (computerChoice == 1) {
+    computerChoice = "rock";
+   } else if (computerChoice == 2) {
+    computerChoice = "paper";
    } else {
-    return "scissors";
+    computerChoice= "scissors";
    }
+   return computerChoice;
 }
 
 //function for users to select their choice
 function getUserChoice () {
-    let UserChoice = prompt ("Rock, paper, or scissors?");
+    let userChoice = prompt ("Rock, paper, or scissors?");
     return UserChoice;
 }
