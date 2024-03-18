8d11e6ae7dc7f038b3fb9174d34e911c352bc9a6
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-18 17:32:38 -0400

    clean up code and comment

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ var computerScore = 0;
 var userScore = 0;
 var computerChoice;
 var userChoice;
-var result;
+var roundResult;
+var finalResult;
 
-// function to randomly return "rock", "paper", or "scissors".
+// function to randomly return from rock(1), paper(2), or scissors(3) 
 function getComputerChoice(){
    var computerChoice = Math.floor(Math.random() * 3) + 1
    if (computerChoice === 1) {
@@ -18,7 +19,7 @@ function getComputerChoice(){
    return computerChoice;
 }
 
-//function for users to select their choice
+//users select their choice
 function getUserChoice () {
     var userChoice = prompt ("rock, paper, or scissors?").toLowerCase();
     return userChoice;
@@ -31,8 +32,10 @@ function playRound () {
     var userChoice = getUserChoice();
     console.log("You've selected " + userChoice);
     console.log("The computer has selected " + computerChoice);
-    // user and computer choosing the same thing
+    // user and computer with same choice
     if (userChoice === "rock" && computerChoice === "rock" || userChoice === "scissors" && computerChoice === "scissors" || userChoice === "paper" && computerChoice === "paper") {
+        userScore = ++userScore;
+        computerScore = ++computerScore;
         roundResult = "It's a tie!";
     } 
     // user wins
@@ -53,8 +56,8 @@ function playRound () {
 
 //10 rounds rps with a final score
 function playGame() {
-    var computerScore = 0;
-    var userScore = 0;
+    computerScore = 0;
+    userScore = 0;
     playRound();
     playRound();
     playRound();
@@ -65,13 +68,15 @@ function playGame() {
     playRound();
     playRound();
     playRound();
-    // show winner
+    // show final score of the game
+    console.log("Your final score is " + userScore + " & the computer's final score is " + computerScore)
+    // show final result of the game
     if (userScore > computerScore) {
-        finalResult = "Yay! You won the game!"
+        finalResult = "Congratulations, you won!"
     } else if (computerScore > userScore) {
-        finalResult = "Sorry, the computer won!"
-    } else {
-        finalResult = "You tied this game, try again!"
+        finalResult = "Sorry, you lost!"
+    } else if (computerScore = userScore) {
+        finalResult = "You tied!"
     }
     console.log(finalResult);
     }
