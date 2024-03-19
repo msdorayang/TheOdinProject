5e7b51d5377106a7419e6c460258a2908b69ac51
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-19 15:55:39 -0400

    complete leap year tests!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 
+        // year is divisible by 4
+        && year % 100 !== 0 || 
+        // and year is NOT divisible by 100
+        year % 400 === 0)
+        // OR - year is divisible by 400
+        {
+            return true;
+        } else {
+            return false;
+        }
 };
 
 // Do not edit below this line
