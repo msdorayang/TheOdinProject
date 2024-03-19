b2ec0affd8353d677140e50ddc15a6565d468c8d
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-19 16:08:54 -0400

    complete C to F temp conversition tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
 const convertToCelsius = function(temp) {
   let converted = (temp -32) * (5/9);
-  let rounded = Math.round(converted * 10) / 10
+  let rounded = Math.round(converted * 10) / 10;
   return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let converted = (temp * (9/5)) + 32;
+  let rounded = Math.round(converted * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
