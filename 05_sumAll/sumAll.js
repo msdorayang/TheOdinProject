af8f73798e704a18fb1badc61e4745b955d0cda4
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-19 15:41:56 -0400

    pass tests for sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum = 0;
+    if (max > min) {
+        for (let step = min; step <= max; step++) {
+            sum += step;
+        }
+    } else if (max < min) {
+        for (let step = max; step <= min; step++) {
+            sum += step;
+        }
+    } else {
+        sum + min;
+    }
+    if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    } else return sum;
 };
 
 // Do not edit below this line
