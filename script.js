1ac7042f3f51d847cf315f268900a47733b49ba7
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-25 18:07:53 -0400

    display final results after 10 rounds

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-// declare variables
+// initialize scores
 var computerScore = 0;
 var userScore = 0;
-var gameOver = false;
 
-// function to randomly return from rock(1), paper(2), or scissors(3) 
-function getComputerChoice(){
-   var computerChoice = Math.floor(Math.random() * 3) + 1
-   if (computerChoice === 1) {
-    computerChoice = "rock";
-   } else if (computerChoice === 2) {
-    computerChoice = "paper";
-   } else {
-    computerChoice = "scissors";
-   }
-   return computerChoice;
-}
-
-// event listener to the buttons that call playRound function with the correct userChoice when a button is clicked
+// event listener to buttons that call playRound function with the correct userChoice when clicked
 const buttons = document.querySelectorAll('.choice');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -25,22 +11,24 @@ buttons.forEach(button => {
     })
 })
 
-//function to play single round
-function playRound (userChoice) {
-    // variations call the function to save the return value of the call
-    var computerChoice = getComputerChoice();
-    var roundResult = result(userChoice, computerChoice);
-    var scoreBoard = "Computer: " + computerScore + "  |  You: " + userScore;
-    var selectionMsg = "You have selected " + userChoice + "<br>The computer has selected " + computerChoice
-    document.getElementById("current-round").innerHTML = selectionMsg + "<br>" + roundResult;
-    document.getElementById("score").innerHTML = "Player: " + userScore + "       |       Computer: " + computerScore;
+// randomly return from rock(1), paper(2), or scissors(3) to generate computerChoice
+function getComputerChoice(){
+    var computerChoice = Math.floor(Math.random() * 3) + 1
+   if (computerChoice === 1) {
+    return "rock";
+   } else if (computerChoice === 2) {
+    return "paper";
+   } else {
+    return "scissors";
+   }
 }
-    // user and computer with same choice
+
+// user and computer with same choice
 function result (userChoice, computerChoice) {
     if (userChoice === "rock" && computerChoice === "rock" || userChoice === "scissors" && computerChoice === "scissors" || userChoice === "paper" && computerChoice === "paper") {
         roundResult = "It's a tie!";
     } 
-    // user wins
+// user wins
     else if (userChoice === "rock" && computerChoice === "scissors" || userChoice === "scissors" && computerChoice === "paper" || userChoice === "paper" && computerChoice === "rock") {
         userScore++;
         roundResult = "You win!";
@@ -49,22 +37,34 @@ function result (userChoice, computerChoice) {
     else if (userChoice === "rock" && computerChoice === "paper" || userChoice === "scissors" && computerChoice === "rock" || userChoice === "paper" && computerChoice === "scissors") {
         computerScore++;
         roundResult = "You lose!";
-    } else {
-        roundResult = "Invalid Entry!";
     }
     return roundResult;
+    }
+
+//end game
+function final(userScore, computerScore) {
+    if (userScore === 10 && computerScore === 10) {
+    return "You and the computer tied!!"
+} else if (userScore === 10 && computerScore != 10) {
+    return "You are the winner!!";
+} else if (computerScore === 10 && userScore != 10) {
+    return "The computer is the winner!!";
+} else {
+    return "";
+}
 }
 
-// End game
-if (userScore === 5 && computerScore === 5) {
-    var gameover = true;
-    document.getElementById("winner").innerHTML="You and the computer tied!!"
-} else if (userScore === 5 && computerScore != 5) {
-    var gameover = true;
-    document.getElementById("winner").innerHTML="You are the winner!!";
-} else if (computerScore === 5 && userScore != 5) {
-    var gameover = true;
-    document.getElementById("winner").innerHTML="The computer is the winner!!";
-} else {
-    var gameover = false;
+//function to play single round
+function playRound (userChoice) {
+    // variations call the function to save the return value of the call
+    var computerChoice = getComputerChoice();
+    var roundResult = result(userChoice, computerChoice);
+    var scoreBoard = "Computer: " + computerScore + "  |  You: " + userScore;
+    var selectionMsg = "You have selected " + userChoice + "<br>The computer has selected " + computerChoice
+    var finalResult = final(userScore, computerScore);
+    document.getElementById("current-round").innerHTML = selectionMsg + "<br>" + roundResult;
+    document.getElementById("score").innerHTML = "Player: " + userScore + "       |       Computer: " + computerScore;
+    document.getElementById("winner").innerHTML = finalResult;
 }
+
+
