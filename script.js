213ee5a619e572a0758b5a207f4bffef5cc9a9ea
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-27 14:16:39 -0400

    set up resize of boxes based on variable

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
+const content = document.getElementById('content');
 const container = document.createElement('div');
 container.id = 'container';
-document.getElementById('content').appendChild(container);
+content.appendChild(container);
 
-for (var j = 0; j < 16; j++) {
-    const row = document.createElement('div');
-    row.className = 'row';
-    row.id = 'row' + (j+1);
-    container.appendChild(row);
-    for (var i = 0; i < 16; i++) {
-        const box = document.createElement('div');
-        box.className = 'box';
-        row.appendChild(box);
+var size = 10;
+function createGrid() {
+    for (var j = 0; j < size; j++) {
+        const row = document.createElement('div');
+        row.className = 'row';
+        row.id = 'row' + (j+1);
+        row.style.height = "calc(32rem/" + size + ")";
+        container.appendChild(row);
+        for (var i = 0; i < size; i++) {
+            const box = document.createElement('div');
+            box.className = 'box';
+            box.style.width = "calc(32rem/" + size + ")";
+            box.style.height = "calc(32rem/" + size + ")";
+            row.appendChild(box);
+        }
     }
+    const boxes = document.querySelectorAll('.box');
+    for (const pixel of boxes) {
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = "salmon";
+        })
 }
-
-const boxes = document.querySelectorAll('.box');
-for (const pixel of boxes) {
-    pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = "salmon";
-    })
 }
+createGrid();
 
 const btn = document.querySelector("button");
 btn.addEventListener("click", () => {
     do {
         var size = parseInt(window.prompt("please eneter a number from 1 to 100", ""), 10);
     } while (isNaN(size) || size > 100 || size < 1);
+    console.log(size);
+    container.innerHTML = '';
+    createGrid();
 })
