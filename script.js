5a8cdfa175a1b4cda60cae67c43cdf0a33106336
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-27 11:10:27 -0400

    create nested loops of 16 rows

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ container.id = 'container';
 container.className = 'container';
 document.getElementsByTagName('body')[0].appendChild(container);
 
-const row = document.createElement('div');
-row.className = 'row';
-row.id = 'row1';
-container.appendChild(row);
-
-for (var i = 0; i < 16; i++) {
-    const box = document.createElement('div');
-    box.className = 'box';
-    row.appendChild(box);
+for (var j = 0; j < 16; j++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+    row.id = 'row' + (j+1);
+    container.appendChild(row);
+    for (var i = 0; i < 16; i++) {
+        const box = document.createElement('div');
+        box.className = 'box';
+        row.appendChild(box);
+    }
 }
