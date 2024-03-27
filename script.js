9b651e415ae8f53107d71418ed67f11f60d95f96
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-27 11:03:41 -0400

    append box in row in container

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const container = document.createElement('div');
 container.id = 'container';
 container.className = 'container';
 document.getElementsByTagName('body')[0].appendChild(container);
+
+const row = document.createElement('div');
+row.className = 'row';
+row.id = 'row1';
+container.appendChild(row);
+
+const box = document.createElement('div');
+box.className = 'box';
+row.appendChild(box);
