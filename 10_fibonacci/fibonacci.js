93054da84150395c72ef1069513210126de44d5d
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-28 17:47:38 -0400

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num === 0) {
+        return 0;
+    }
+    else if (num < 0) {
+        return "OOPS";
+    }
+    else {
+    const fibs = [1,1];
+    for (i = 1; i <= num-2; i++) {
+        var currentFib = fibs[i];
+        var previousFib = fibs[i-1];
+        var nextFib = currentFib + previousFib;
+        fibs.push(nextFib);
+    }
+    return fibs[fibs.length-1];
+}};
 // Do not edit below this line
 module.exports = fibonacci;
