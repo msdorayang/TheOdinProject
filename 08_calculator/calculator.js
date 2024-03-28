484d841d2a2dbe7f87601c7778b1dc6b07dbd8ce
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-03-28 15:16:10 -0400

    test calculator functions pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1)
+  return 1;
+// start the loop one less than the input value, and decrement i after each iteration
+  for (var i = a - 1; i >=1; i--) {
+    // store the value of a at each iteration
+    a = a * i;
+  }
+  return a;
 };
 
 // Do not edit below this line
