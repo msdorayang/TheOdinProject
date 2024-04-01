a0116ebda2d140980af7227f79d38530939ad63e
dorayang <128941109-msdorayang@users.noreply.github.com>
2024-04-01 16:05:49 -0400

    update display value on screen dynamically

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ let displayValue = 0;
 const display = document.querySelector(".display");
 display.innerHTML = displayValue;
 
+// clear button function
+const clear = document.querySelector(".clear");
+function clearAll() {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    displayValue = 0;
+    display.innerHTML = displayValue;
+}
+clear.addEventListener("click", clearAll);
+
 // digit buttons and operators
 const digits = document.querySelectorAll(".digit");
 const operators = document.querySelectorAll(".operator");
@@ -42,36 +53,36 @@ function add(a, b) {
     num1 = parseInt(a) + parseInt(b);
     displayValue = num1;
     display.innerHTML = displayValue;
+    num2 = '';
+    operator = '';
     return num1;
   };
 function subtract (a, b) {
     num1 = parseInt(a) - parseInt(b);
     displayValue = num1;
     display.innerHTML = displayValue;
+    num2 = '';
+    operator = '';
     return num1;
   };
 function multiply(a, b) {
     num1 = parseInt(a) * parseInt(b);
     displayValue = num1;
     display.innerHTML = displayValue;
+    num2 = '';
+    operator = '';
     return num1;
   };
 function divide(a, b) {
     num1 = parseInt(a) / parseInt(b);
     displayValue = num1;
     display.innerHTML = displayValue;
+    num2 = '';
+    operator = '';
     return num1;
 }
 
-// clear button function
-const clear = document.querySelector(".clear");
-function clearAll() {
-    num1 = '';
-    num2 = '';
-    operator = '';
-    displayValue = 0;
-}
-clear.addEventListener("click", clearAll);
+
 
 // execute function using equal sign
 const equal = document.querySelector(".equal");
